917b2117be7802a4de8f38e5df402d23cabc41a1
CappelliLeonardo <l.cappelli1995@gmail.com>
2023-12-14 12:13:56 +0100

    creato array e ciclo for

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
+// Consegna
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta:
+// le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba,
+// perciò nell'array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri
+// generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo
+// possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che
+// l'utente ha cliccato su una cella che non era una bomba.
 
+// Consigli del giorno: :party_wizard:
+// Scriviamo prima cosa vogliamo fare passo passo in italiano,
+// dividiamo il lavoro in micro problemi.
+// Ad esempio:
+// Di cosa ho bisogno per generare i numeri?
+// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+// Le validazioni e i controlli possiamo farli anche in un secondo momento.
 
 const buttonInput = document.querySelector('button');
 
@@ -7,6 +25,18 @@ const quadratone = document.querySelector ('.quadratone');
 const diffSelect = document.getElementById('difficulty');
 console.log('diffSelect', diffSelect, typeof diffSelect);
 
+const bomb = [];
+
+
+// ciclo for per creare 16 numeri casuali
+
+for (let i = 0; i < 16; i++) {
+    const randomBomb = generateRandomNumber(1, 16);
+    console.log(randomBomb);
+
+    bomb.push (randomBomb);
+    
+}
 
 
 buttonInput.addEventListener('click', function(){
@@ -41,3 +71,11 @@ buttonInput.addEventListener('click', function(){
     }
 }
 );
+
+// FUNZIONE 
+
+function generateRandomNumber (min, max) {
+    const randNUm = Math.floor (Math.random () * (max - min + 1)) + min;
+
+    return randNUm;
+}
